48f4542376b19cab795e03110987958fb1f0e031
anthonyaubrey <anthonyaubrey245@gmail.com>
2016-04-29 11:14:01 -0400

    added js animation slide

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $('.barmove-org').on('click', function (e) {
   $('.barmove-blue').addClass('slide');
 });
+
+$('.barmove-other').on('click', function (e) {
+  $('.barmove-woo').addClass('slide');
+});
